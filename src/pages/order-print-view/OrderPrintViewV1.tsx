4681990e8d663807bf1398bcf20cf83ecc3941b7
smallofficesoftware <app.craftbox@gmail.com>
2026-08-12 19:32:10 +0530

    Update order print views, env files, and task chat

--- src/pages/order-print-view/OrderPrintViewV1.tsx
+++ src/pages/order-print-view/OrderPrintViewV1.tsx
@@ -882,7 +882,7 @@ const OrderPrintViewV1 = () => {
   const loadWha = async () => {
     const a_application_login_id = getID || localStorage.getItem("UUID");
     const response = await getWhatsappFlag(a_application_login_id);
-    setWhatsappConfigDetail(response.WHATSAPP_PLATEFORM);
+    setWhatsappConfigDetail(response?.WHATSAPP_PLATEFORM);
   };
   const loadRights = async () => {
     const a_application_login_id = getID || localStorage.getItem("UUID");
